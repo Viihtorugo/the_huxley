4636131c2dc2b25ef2173ca04e03b42c3d79ccd5
Viihtorugo <victor-torugo@hotmail.com>
2025-01-31 11:11:33 -0300

    [obi]: solved the carteiro

--- 4_level/0684_carteiro.cpp
+++ 4_level/0684_carteiro.cpp
@@ -4,47 +4,36 @@ using namespace std;
 
 int main()
 {
+
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(0);
+
     int n, m;
     cin >> n >> m;
 
     int house[n];
 
-    unordered_map<int, unordered_map<int, int>> dist;
     unordered_map<int, int> index;
 
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &house[i]);
+        cin >> house[i];
         index[house[i]] = i;
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        int c = 0;
-        for (int j = i; j >= 0; j--)
-        {
-            dist[i][j] = c;
-            c++;
-        }
 
-        c = 1;
-        for (int j = i + 1; j < n; j++)
-        {
-            dist[i][j] = c;
-            c++;
-        }
-    }
-
-    int count = 0, pos = 0, num;
+    long long count = 0;
+    int pos = 0, num;
     for (int i = 0; i < m; i++)
     {
-        scanf("%d", &num);
+        cin >> num;
 
-        if (dist.find(pos) != dist.end())
+        if (index.find(num) != index.end())
         {
-            count += dist[pos][index[num]];
+            count += abs(pos - index[num]);
             pos = index[num];
         }
+
     }
 
     cout << count << '\n';
